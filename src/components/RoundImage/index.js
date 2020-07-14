dd9453c9c69fc1cfd18803b51b7dcd9cd0e25cf1
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-07-14 19:53:07 +0800

    Created RoundImage component

---
+++ src/components/RoundImage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "react-bootstrap"
+
+function RoundImage() {
+    return (
+        <Image src="../../assets/profile.jpg" alt="Profile image" roundedCircle />
+    )
+}
+
+export default RoundImage;
